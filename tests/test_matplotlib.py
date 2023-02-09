a784400c071d68690550423ef439c69656c8255c
gen740 <genfujimoto@gmail.com>
2023-02-09 19:07:57 +0900

    Update

---
+++ tests/test_matplotlib.py
@@ -0,0 +1,1312 @@
+from io import BytesIO
+
+import matplotlib.pyplot as plt
+
+
+def save_static_image() -> None:
+    plt.savefig(BytesIO())
+
+
+def test1():
+    plt.figure()
+    save_static_image()
+
+
+def test2():
+    plt.figure()
+    save_static_image()
+
+
+def test3():
+    plt.figure()
+    save_static_image()
+
+
+def test4():
+    plt.figure()
+    save_static_image()
+
+
+def test5():
+    plt.figure()
+    save_static_image()
+
+
+def test6():
+    plt.figure()
+    save_static_image()
+
+
+def test7():
+    plt.figure()
+    save_static_image()
+
+
+def test8():
+    plt.figure()
+    save_static_image()
+
+
+def test9():
+    plt.figure()
+    save_static_image()
+
+
+def test10():
+    plt.figure()
+    save_static_image()
+
+
+def test11():
+    plt.figure()
+    save_static_image()
+
+
+def test12():
+    plt.figure()
+    save_static_image()
+
+
+def test13():
+    plt.figure()
+    save_static_image()
+
+
+def test14():
+    plt.figure()
+    save_static_image()
+
+
+def test15():
+    plt.figure()
+    save_static_image()
+
+
+def test16():
+    plt.figure()
+    save_static_image()
+
+
+def test17():
+    plt.figure()
+    save_static_image()
+
+
+def test18():
+    plt.figure()
+    save_static_image()
+
+
+def test19():
+    plt.figure()
+    save_static_image()
+
+
+def test20():
+    plt.figure()
+    save_static_image()
+
+
+def test21():
+    plt.figure()
+    save_static_image()
+
+
+def test22():
+    plt.figure()
+    save_static_image()
+
+
+def test23():
+    plt.figure()
+    save_static_image()
+
+
+def test24():
+    plt.figure()
+    save_static_image()
+
+
+def test25():
+    plt.figure()
+    save_static_image()
+
+
+def test26():
+    plt.figure()
+    save_static_image()
+
+
+def test27():
+    plt.figure()
+    save_static_image()
+
+
+def test28():
+    plt.figure()
+    save_static_image()
+
+
+def test29():
+    plt.figure()
+    save_static_image()
+
+
+def test30():
+    plt.figure()
+    save_static_image()
+
+
+def test31():
+    plt.figure()
+    save_static_image()
+
+
+def test32():
+    plt.figure()
+    save_static_image()
+
+
+def test33():
+    plt.figure()
+    save_static_image()
+
+
+def test34():
+    plt.figure()
+    save_static_image()
+
+
+def test35():
+    plt.figure()
+    save_static_image()
+
+
+def test36():
+    plt.figure()
+    save_static_image()
+
+
+def test37():
+    plt.figure()
+    save_static_image()
+
+
+def test38():
+    plt.figure()
+    save_static_image()
+
+
+def test39():
+    plt.figure()
+    save_static_image()
+
+
+def test40():
+    plt.figure()
+    save_static_image()
+
+
+def test41():
+    plt.figure()
+    save_static_image()
+
+
+def test42():
+    plt.figure()
+    save_static_image()
+
+
+def test43():
+    plt.figure()
+    save_static_image()
+
+
+def test44():
+    plt.figure()
+    save_static_image()
+
+
+def test45():
+    plt.figure()
+    save_static_image()
+
+
+def test46():
+    plt.figure()
+    save_static_image()
+
+
+def test47():
+    plt.figure()
+    save_static_image()
+
+
+def test48():
+    plt.figure()
+    save_static_image()
+
+
+def test49():
+    plt.figure()
+    save_static_image()
+
+
+def test50():
+    plt.figure()
+    save_static_image()
+
+
+def test51():
+    plt.figure()
+    save_static_image()
+
+
+def test52():
+    plt.figure()
+    save_static_image()
+
+
+def test53():
+    plt.figure()
+    save_static_image()
+
+
+def test54():
+    plt.figure()
+    save_static_image()
+
+
+def test55():
+    plt.figure()
+    save_static_image()
+
+
+def test56():
+    plt.figure()
+    save_static_image()
+
+
+def test57():
+    plt.figure()
+    save_static_image()
+
+
+def test58():
+    plt.figure()
+    save_static_image()
+
+
+def test59():
+    plt.figure()
+    save_static_image()
+
+
+def test60():
+    plt.figure()
+    save_static_image()
+
+
+def test61():
+    plt.figure()
+    save_static_image()
+
+
+def test62():
+    plt.figure()
+    save_static_image()
+
+
+def test63():
+    plt.figure()
+    save_static_image()
+
+
+def test64():
+    plt.figure()
+    save_static_image()
+
+
+def test65():
+    plt.figure()
+    save_static_image()
+
+
+def test66():
+    plt.figure()
+    save_static_image()
+
+
+def test67():
+    plt.figure()
+    save_static_image()
+
+
+def test68():
+    plt.figure()
+    save_static_image()
+
+
+def test69():
+    plt.figure()
+    save_static_image()
+
+
+def test70():
+    plt.figure()
+    save_static_image()
+
+
+def test71():
+    plt.figure()
+    save_static_image()
+
+
+def test72():
+    plt.figure()
+    save_static_image()
+
+
+def test73():
+    plt.figure()
+    save_static_image()
+
+
+def test74():
+    plt.figure()
+    save_static_image()
+
+
+def test75():
+    plt.figure()
+    save_static_image()
+
+
+def test76():
+    plt.figure()
+    save_static_image()
+
+
+def test77():
+    plt.figure()
+    save_static_image()
+
+
+def test78():
+    plt.figure()
+    save_static_image()
+
+
+def test79():
+    plt.figure()
+    save_static_image()
+
+
+def test80():
+    plt.figure()
+    save_static_image()
+
+
+def test81():
+    plt.figure()
+    save_static_image()
+
+
+def test82():
+    plt.figure()
+    save_static_image()
+
+
+def test83():
+    plt.figure()
+    save_static_image()
+
+
+def test84():
+    plt.figure()
+    save_static_image()
+
+
+def test85():
+    plt.figure()
+    save_static_image()
+
+
+def test86():
+    plt.figure()
+    save_static_image()
+
+
+def test87():
+    plt.figure()
+    save_static_image()
+
+
+def test88():
+    plt.figure()
+    save_static_image()
+
+
+def test89():
+    plt.figure()
+    save_static_image()
+
+
+def test90():
+    plt.figure()
+    save_static_image()
+
+
+def test91():
+    plt.figure()
+    save_static_image()
+
+
+def test92():
+    plt.figure()
+    save_static_image()
+
+
+def test93():
+    plt.figure()
+    save_static_image()
+
+
+def test94():
+    plt.figure()
+    save_static_image()
+
+
+def test95():
+    plt.figure()
+    save_static_image()
+
+
+def test96():
+    plt.figure()
+    save_static_image()
+
+
+def test97():
+    plt.figure()
+    save_static_image()
+
+
+def test98():
+    plt.figure()
+    save_static_image()
+
+
+def test99():
+    plt.figure()
+    save_static_image()
+
+
+def test100():
+    plt.figure()
+    save_static_image()
+
+
+def test101():
+    plt.figure()
+    save_static_image()
+
+
+def test102():
+    plt.figure()
+    save_static_image()
+
+
+def test103():
+    plt.figure()
+    save_static_image()
+
+
+def test104():
+    plt.figure()
+    save_static_image()
+
+
+def test105():
+    plt.figure()
+    save_static_image()
+
+def test106():
+    plt.figure()
+    save_static_image()
+
+def test107():
+    plt.figure()
+    save_static_image()
+
+def test108():
+    plt.figure()
+    save_static_image()
+
+def test109():
+    plt.figure()
+    save_static_image()
+
+def test110():
+    plt.figure()
+    save_static_image()
+
+def test111():
+    plt.figure()
+    save_static_image()
+
+def test112():
+    plt.figure()
+    save_static_image()
+
+def test113():
+    plt.figure()
+    save_static_image()
+
+def test114():
+    plt.figure()
+    save_static_image()
+
+def test115():
+    plt.figure()
+    save_static_image()
+
+def test116():
+    plt.figure()
+    save_static_image()
+
+def test117():
+    plt.figure()
+    save_static_image()
+
+def test118():
+    plt.figure()
+    save_static_image()
+
+def test119():
+    plt.figure()
+    save_static_image()
+
+def test120():
+    plt.figure()
+    save_static_image()
+
+def test121():
+    plt.figure()
+    save_static_image()
+
+def test122():
+    plt.figure()
+    save_static_image()
+
+def test123():
+    plt.figure()
+    save_static_image()
+
+def test124():
+    plt.figure()
+    save_static_image()
+
+def test125():
+    plt.figure()
+    save_static_image()
+
+def test126():
+    plt.figure()
+    save_static_image()
+
+def test127():
+    plt.figure()
+    save_static_image()
+
+def test128():
+    plt.figure()
+    save_static_image()
+
+def test129():
+    plt.figure()
+    save_static_image()
+
+def test130():
+    plt.figure()
+    save_static_image()
+
+def test131():
+    plt.figure()
+    save_static_image()
+
+def test132():
+    plt.figure()
+    save_static_image()
+
+def test133():
+    plt.figure()
+    save_static_image()
+
+def test134():
+    plt.figure()
+    save_static_image()
+
+def test135():
+    plt.figure()
+    save_static_image()
+
+def test136():
+    plt.figure()
+    save_static_image()
+
+def test137():
+    plt.figure()
+    save_static_image()
+
+def test138():
+    plt.figure()
+    save_static_image()
+
+def test139():
+    plt.figure()
+    save_static_image()
+
+def test140():
+    plt.figure()
+    save_static_image()
+
+def test141():
+    plt.figure()
+    save_static_image()
+
+def test142():
+    plt.figure()
+    save_static_image()
+
+def test143():
+    plt.figure()
+    save_static_image()
+
+def test144():
+    plt.figure()
+    save_static_image()
+
+def test145():
+    plt.figure()
+    save_static_image()
+
+def test146():
+    plt.figure()
+    save_static_image()
+
+def test147():
+    plt.figure()
+    save_static_image()
+
+def test148():
+    plt.figure()
+    save_static_image()
+
+def test149():
+    plt.figure()
+    save_static_image()
+
+def test150():
+    plt.figure()
+    save_static_image()
+
+def test151():
+    plt.figure()
+    save_static_image()
+
+def test152():
+    plt.figure()
+    save_static_image()
+
+def test153():
+    plt.figure()
+    save_static_image()
+
+def test154():
+    plt.figure()
+    save_static_image()
+
+def test155():
+    plt.figure()
+    save_static_image()
+
+def test156():
+    plt.figure()
+    save_static_image()
+
+def test157():
+    plt.figure()
+    save_static_image()
+
+def test158():
+    plt.figure()
+    save_static_image()
+
+def test159():
+    plt.figure()
+    save_static_image()
+
+def test160():
+    plt.figure()
+    save_static_image()
+
+def test161():
+    plt.figure()
+    save_static_image()
+
+def test162():
+    plt.figure()
+    save_static_image()
+
+def test163():
+    plt.figure()
+    save_static_image()
+
+def test164():
+    plt.figure()
+    save_static_image()
+
+def test165():
+    plt.figure()
+    save_static_image()
+
+def test166():
+    plt.figure()
+    save_static_image()
+
+def test167():
+    plt.figure()
+    save_static_image()
+
+def test168():
+    plt.figure()
+    save_static_image()
+
+def test169():
+    plt.figure()
+    save_static_image()
+
+def test170():
+    plt.figure()
+    save_static_image()
+
+def test171():
+    plt.figure()
+    save_static_image()
+
+def test172():
+    plt.figure()
+    save_static_image()
+
+def test173():
+    plt.figure()
+    save_static_image()
+
+def test174():
+    plt.figure()
+    save_static_image()
+
+def test175():
+    plt.figure()
+    save_static_image()
+
+def test176():
+    plt.figure()
+    save_static_image()
+
+def test177():
+    plt.figure()
+    save_static_image()
+
+def test178():
+    plt.figure()
+    save_static_image()
+
+def test179():
+    plt.figure()
+    save_static_image()
+
+def test180():
+    plt.figure()
+    save_static_image()
+
+def test181():
+    plt.figure()
+    save_static_image()
+
+def test182():
+    plt.figure()
+    save_static_image()
+
+def test183():
+    plt.figure()
+    save_static_image()
+
+def test184():
+    plt.figure()
+    save_static_image()
+
+def test185():
+    plt.figure()
+    save_static_image()
+
+def test186():
+    plt.figure()
+    save_static_image()
+
+def test187():
+    plt.figure()
+    save_static_image()
+
+def test188():
+    plt.figure()
+    save_static_image()
+
+def test189():
+    plt.figure()
+    save_static_image()
+
+def test190():
+    plt.figure()
+    save_static_image()
+
+def test191():
+    plt.figure()
+    save_static_image()
+
+def test192():
+    plt.figure()
+    save_static_image()
+
+def test193():
+    plt.figure()
+    save_static_image()
+
+def test194():
+    plt.figure()
+    save_static_image()
+
+def test195():
+    plt.figure()
+    save_static_image()
+
+def test196():
+    plt.figure()
+    save_static_image()
+
+def test197():
+    plt.figure()
+    save_static_image()
+
+def test198():
+    plt.figure()
+    save_static_image()
+
+def test199():
+    plt.figure()
+    save_static_image()
+
+def test200():
+    plt.figure()
+    save_static_image()
+
+def test201():
+    plt.figure()
+    save_static_image()
+
+def test202():
+    plt.figure()
+    save_static_image()
+
+def test203():
+    plt.figure()
+    save_static_image()
+
+def test204():
+    plt.figure()
+    save_static_image()
+
+def test205():
+    plt.figure()
+    save_static_image()
+
+def test206():
+    plt.figure()
+    save_static_image()
+
+def test207():
+    plt.figure()
+    save_static_image()
+
+def test208():
+    plt.figure()
+    save_static_image()
+
+def test209():
+    plt.figure()
+    save_static_image()
+
+def test210():
+    plt.figure()
+    save_static_image()
+
+def test211():
+    plt.figure()
+    save_static_image()
+
+def test212():
+    plt.figure()
+    save_static_image()
+
+def test213():
+    plt.figure()
+    save_static_image()
+
+def test214():
+    plt.figure()
+    save_static_image()
+
+def test215():
+    plt.figure()
+    save_static_image()
+
+def test216():
+    plt.figure()
+    save_static_image()
+
+def test217():
+    plt.figure()
+    save_static_image()
+
+def test218():
+    plt.figure()
+    save_static_image()
+
+def test219():
+    plt.figure()
+    save_static_image()
+
+def test220():
+    plt.figure()
+    save_static_image()
+
+def test221():
+    plt.figure()
+    save_static_image()
+
+def test222():
+    plt.figure()
+    save_static_image()
+
+def test223():
+    plt.figure()
+    save_static_image()
+
+def test224():
+    plt.figure()
+    save_static_image()
+
+def test225():
+    plt.figure()
+    save_static_image()
+
+def test226():
+    plt.figure()
+    save_static_image()
+
+def test227():
+    plt.figure()
+    save_static_image()
+
+def test228():
+    plt.figure()
+    save_static_image()
+
+def test229():
+    plt.figure()
+    save_static_image()
+
+def test230():
+    plt.figure()
+    save_static_image()
+
+def test231():
+    plt.figure()
+    save_static_image()
+
+def test232():
+    plt.figure()
+    save_static_image()
+
+def test233():
+    plt.figure()
+    save_static_image()
+
+def test234():
+    plt.figure()
+    save_static_image()
+
+def test235():
+    plt.figure()
+    save_static_image()
+
+def test236():
+    plt.figure()
+    save_static_image()
+
+def test237():
+    plt.figure()
+    save_static_image()
+
+def test238():
+    plt.figure()
+    save_static_image()
+
+def test239():
+    plt.figure()
+    save_static_image()
+
+def test240():
+    plt.figure()
+    save_static_image()
+
+def test241():
+    plt.figure()
+    save_static_image()
+
+def test242():
+    plt.figure()
+    save_static_image()
+
+def test243():
+    plt.figure()
+    save_static_image()
+
+def test244():
+    plt.figure()
+    save_static_image()
+
+def test245():
+    plt.figure()
+    save_static_image()
+
+def test246():
+    plt.figure()
+    save_static_image()
+
+def test247():
+    plt.figure()
+    save_static_image()
+
+def test248():
+    plt.figure()
+    save_static_image()
+
+def test249():
+    plt.figure()
+    save_static_image()
+
+def test250():
+    plt.figure()
+    save_static_image()
+
+def test251():
+    plt.figure()
+    save_static_image()
+
+def test252():
+    plt.figure()
+    save_static_image()
+
+def test253():
+    plt.figure()
+    save_static_image()
+
+def test254():
+    plt.figure()
+    save_static_image()
+
+def test255():
+    plt.figure()
+    save_static_image()
+
+def test256():
+    plt.figure()
+    save_static_image()
+
+def test257():
+    plt.figure()
+    save_static_image()
+
+def test258():
+    plt.figure()
+    save_static_image()
+
+def test259():
+    plt.figure()
+    save_static_image()
+
+def test260():
+    plt.figure()
+    save_static_image()
+
+def test261():
+    plt.figure()
+    save_static_image()
+
+def test262():
+    plt.figure()
+    save_static_image()
+
+def test263():
+    plt.figure()
+    save_static_image()
+
+def test264():
+    plt.figure()
+    save_static_image()
+
+def test265():
+    plt.figure()
+    save_static_image()
+
+def test266():
+    plt.figure()
+    save_static_image()
+
+def test267():
+    plt.figure()
+    save_static_image()
+
+def test268():
+    plt.figure()
+    save_static_image()
+
+def test269():
+    plt.figure()
+    save_static_image()
+
+def test270():
+    plt.figure()
+    save_static_image()
+
+def test271():
+    plt.figure()
+    save_static_image()
+
+def test272():
+    plt.figure()
+    save_static_image()
+
+def test273():
+    plt.figure()
+    save_static_image()
+
+def test274():
+    plt.figure()
+    save_static_image()
+
+def test275():
+    plt.figure()
+    save_static_image()
+
+def test276():
+    plt.figure()
+    save_static_image()
+
+def test277():
+    plt.figure()
+    save_static_image()
+
+def test278():
+    plt.figure()
+    save_static_image()
+
+def test279():
+    plt.figure()
+    save_static_image()
+
+def test280():
+    plt.figure()
+    save_static_image()
+
+def test281():
+    plt.figure()
+    save_static_image()
+
+def test282():
+    plt.figure()
+    save_static_image()
+
+def test283():
+    plt.figure()
+    save_static_image()
+
+def test284():
+    plt.figure()
+    save_static_image()
+
+def test285():
+    plt.figure()
+    save_static_image()
+
+def test286():
+    plt.figure()
+    save_static_image()
+
+def test287():
+    plt.figure()
+    save_static_image()
+
+def test288():
+    plt.figure()
+    save_static_image()
+
+def test289():
+    plt.figure()
+    save_static_image()
+
+def test290():
+    plt.figure()
+    save_static_image()
+
+def test291():
+    plt.figure()
+    save_static_image()
+
+def test292():
+    plt.figure()
+    save_static_image()
+
+def test293():
+    plt.figure()
+    save_static_image()
+
+def test294():
+    plt.figure()
+    save_static_image()
+
+def test295():
+    plt.figure()
+    save_static_image()
+
+def test296():
+    plt.figure()
+    save_static_image()
+
+def test297():
+    plt.figure()
+    save_static_image()
+
+def test298():
+    plt.figure()
+    save_static_image()
+
+def test299():
+    plt.figure()
+    save_static_image()
+
+def test300():
+    plt.figure()
+    save_static_image()
