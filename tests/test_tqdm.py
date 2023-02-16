f3fb613c87ce687845618c564fb33f4e8c2e896e
gen740 <genfujimoto@gmail.com>
2023-02-16 17:43:49 +0900

    Update

--- tests/test_tqdm.py
+++ tests/test_tqdm.py
@@ -1,12 +1,13 @@
 import datetime
 import random
 import time
+import sys
 
 import pytest
 from tqdm import tqdm
 
 
-@pytest.mark.parametrize("n", [i for i in range(500)])
+@pytest.mark.parametrize("n", [i for i in range(100)])
 def test_tqdm(n: int, capsys: pytest.CaptureFixture):
     _ = n
 
@@ -17,7 +18,7 @@ def test_tqdm(n: int, capsys: pytest.CaptureFixture):
 
 
 def tqdm_main():
-    timeout = 2
+    timeout = 1
     total = tqdm.format_interval(100)
     fmt = "{desc} {percentage:3.0f}%|{bar}| {elapsed}/" + total
     progress_bar = tqdm(total=timeout, bar_format=fmt)
@@ -33,12 +34,13 @@ def tqdm_main():
             time_diff -= elapsed_seconds - timeout
 
         progress_bar.update(time_diff)
+        print("*" * 1000, file=sys.stderr)
         last_elapsed_seconds = elapsed_seconds
 
         if elapsed_seconds > timeout:
             break
 
-        time.sleep(random.random() * 0.2)
+        time.sleep(random.random() * 0.1)
 
 
 if __name__ == "__main__":
