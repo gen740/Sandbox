8a7f607e8ea9c2043d866173bec987b775ff1895
gen740 <genfujimoto@gmail.com>
2023-03-07 19:14:59 +0900

    Update

--- tests/test_mxnet.py
+++ tests/test_mxnet.py
@@ -1,1 +1,31 @@
-import mxnet as mx
+from typing import List, Self
+
+import pytest
+
+
+class TempFile:
+    counter = 0
+
+    def __call__(self) -> Self:
+        self.counter = TempFile.counter
+        TempFile.counter += 1
+        return self
+
+    def __str__(self) -> str:
+        return str(self.counter)
+
+    def __del__(self):
+        print(f"deleted {self.counter}")
+
+
+class TemplateFileRAII:
+    tempfile_pool: List[TempFile] = []
+
+    def __call__(self):
+        temp = TempFile()
+        TemplateFileRAII.tempfile_pool.append(temp)
+        return temp
+
+
+def test_description():
+    print(pytest.mark.foo)
